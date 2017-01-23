c62549410553628de8d49013202be2cf114d7f94
AlexNaupay <alexnaupay@gmail.com>
2017-01-22 19:32:15 -0500

    Change App for support JSPs

--- src/main/java/com/devlabs/DemobootApplication.java
+++ src/main/java/com/devlabs/DemobootApplication.java
@@ -2,9 +2,16 @@ package com.devlabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemobootApplication {
+public class DemobootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(DemobootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemobootApplication.class, args);
